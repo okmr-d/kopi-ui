7f32b959fa0fbb8cd04386b18211e8036c78de0e
Daiki Okumura <daiki.okumura@gmail.com>
2024-12-04 01:51:00 +0900

    fix multiple select

--- apps/www/src/components/registry/examples/native-select/native-select-multiple.tsx
+++ apps/www/src/components/registry/examples/native-select/native-select-multiple.tsx
@@ -4,34 +4,34 @@ export default function NativeSelectDemo() {
   return (
     <div className="grid w-full max-w-xs items-center gap-4">
       <NativeSelect multiple className="h-40 ps-0 pe-0 py-2">
-        <option value="1" className="px-3 py-1">
+        <option value="1" className="px-3 py-2">
           Option 1
         </option>
-        <option value="2" className="px-3 py-1">
+        <option value="2" className="px-3 py-2">
           Option 2
         </option>
-        <option value="3" className="px-3 py-1">
+        <option value="3" className="px-3 py-2">
           Option 3
         </option>
-        <option value="4" className="px-3 py-1">
+        <option value="4" className="px-3 py-2">
           Option 4
         </option>
-        <option value="5" className="px-3 py-1">
+        <option value="5" className="px-3 py-2">
           Option 5
         </option>
-        <option value="6" className="px-3 py-1">
+        <option value="6" className="px-3 py-2">
           Option 6
         </option>
-        <option value="7" className="px-3 py-1">
+        <option value="7" className="px-3 py-2">
           Option 7
         </option>
-        <option value="8" className="px-3 py-1">
+        <option value="8" className="px-3 py-2">
           Option 8
         </option>
-        <option value="9" className="px-3 py-1">
+        <option value="9" className="px-3 py-2">
           Option 9
         </option>
-        <option value="10" className="px-3 py-1">
+        <option value="10" className="px-3 py-2">
           Option 10
         </option>
       </NativeSelect>
